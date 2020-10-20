1e42377e0e693947cf6b0e337b8d2fdce5df0ae9
Danielle Thompson <danithompson74@gmail.com>
2020-10-20 15:01:32 -0700

    add hide/show methods

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,17 @@
 $(document).ready(function() {
   $(".clickable").click(function() {
     $(".walrus-showing").fadeIn();
+    $("#walrus-hidden").hide();
   });
   $("#hide-walrus").click(function() {
     $(".walrus-showing").fadeOut();
+    $("#walrus-hidden").show();
   });
+
+
+
+  // $("h1").click(function() {
+  //   $("#peek-a-boo").append("<li>test<li>").addClass("test");
+  // })
 
 });
